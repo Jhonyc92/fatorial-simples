30c168cc5452bf43f3aa091b643051c332252991
Jonathas <114630135+Jhonyc92@users.noreply.github.com>
2025-03-05 03:20:17 -0300

    Update main.py
    Versão 2.0:
    
    Multiplica o valor atual de 'fatorial' pelo número atual na iteração
    
    Imprime o número da iteração atual seguido de "!" e o símbolo de igual
    
    Loop para imprimir a expressão do fatorial (e.g., "1 * 2 * 3")

--- main.py
+++ main.py
@@ -11,14 +11,31 @@ else:
     
     # Inicializa uma variável para armazenar o fatorial do número
     fatorial = 1
+    
+    # Imprime a mensagem inicial indicando o cálculo do fatorial
+    print("Cálculo do fatorial de", numero, ":")
 
-    # Loop que percorre todos os números inteiros de 1 até o número informado pelo usuário (inclusive)
-    for i in range(1, numero + 1):
+    # Loop para multiplicar os números de 1 até o número informado
+    for multiplicador in range(1, numero + 1):
         
-        # Multiplica o valor atual de 'fatorial' pelo número atual (i) na iteração
-        # fatorial = fatorial * i
-        fatorial *= i
+        # Multiplica o valor atual de 'fatorial' pelo número atual na iteração
+        fatorial *= multiplicador
+        
+        # Imprime o número da iteração atual seguido de "!" e o símbolo de igual
+        print(f"{multiplicador}! =", end=" ")
+
+        # Loop para imprimir a expressão do fatorial (e.g., "1 * 2 * 3")
+        for i in range(1, multiplicador + 1):
+            
+            print(i, end="")
+            
+            if i != multiplicador:
+                
+                # Imprime o símbolo de multiplicação se não for o último número
+                print(" * ", end="")
+                
+        # Imprime o resultado parcial do fatorial para a iteração atual
+        print(" = ", fatorial)
 
     # Imprime o resultado final, que é o fatorial do número informado pelo usuário
     print("O fatorial de", numero, "é:", fatorial)
-    
